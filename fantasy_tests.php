8ee136b3be033aa5e6e34ef6c42a81193f9e48ec
Matt Thommes <mattthommes@Matts-MacBook-Air.local>
2018-04-15 08:15:00 -0500

    Yahoo OAuth2 script authorize process working now

--- fantasy_tests.php
+++ fantasy_tests.php
@@ -4,16 +4,39 @@
 
 	use MattThommes\Rest\Yahoo;
 	use MattThommes\Debug;
+
+	if (!session_id()) {
+		session_start();
+	}
+
 	$debug = new Debug;
 
 	function dbg($x, $continue = 0) {
 		return $GLOBALS["debug"]->dbg($x, $continue);
 	}
 
-	$consumerKey = "";
+	$consumerKey = "--";
 	$consumerSecret = "";
-	$callbackUrl = "http://homestead.app/yahoo_api/fantasy_tests.php";
+	$callbackUrl = "https://9f9ad9ca.ngrok.io/yahoo_api/fantasy_tests.php";
+	$accessToken = "";
 
-	$yahoo = new Yahoo($consumerKey, $consumerSecret, $callbackUrl);
+	$yahoo = new Yahoo($consumerKey, $consumerSecret, $callbackUrl, $accessToken);
+
+	if (! $yahoo->getAccessToken()) {
+
+		?>
+		<p><a href="<?php echo $yahoo->getAuthorizeUrl(); ?>">Authorize</a></p>
+		<?php
+
+	} else {
+
+		?>
+		<p>Access token (save in script!):
+			<br><br>
+			<textarea><?php echo $yahoo->getAccessToken(); ?></textarea>
+		</p>
+		<?php
+
+	}
 
 ?>
